ae2fe02520d0ba5dadc9c8c807f511e214a25d93
VDung632 <nguyenvietdung632@gmail.com>
2023-12-23 10:43:24 +0700

    add comment

--- src/routes/Task.js
+++ src/routes/Task.js
@@ -3,10 +3,13 @@ const router = express.Router()
 
 const { getTaskPage, TaskCreate, ModTaskContent, getTask, deleteTask } = require('../controllers/TaskController')
 
-router.get("/Projects/task.html", getTaskPage)
-router.post("/Projects/Tasks/Creation", TaskCreate)
-router.get("/Projects/Tasks/", getTask)
-router.post("/Projects/Tasks/Content", ModTaskContent)
+router.get("/Projects/Tasks/task.html", getTaskPage)
+router.post("/Projects/Tasks", TaskCreate)
+router.get("/Projects/Tasks", getTask)
+router.put("/Projects/Tasks", ModTaskContent)
 router.post("/Projects/Tasks/Delete", deleteTask)
+
+// "/Projects"
+// "/Projects/Project"
 
 module.exports = router
